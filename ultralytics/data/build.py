8edec422abe32941d7f5b2f8bd0fb944f0652c7d
Khaled-Mohammed-Abdelgaber <66785999+Khaled-Mohammed-Abdelgaber@users.noreply.github.com>
2024-08-16 16:32:49 +0300

    Update build.py
    add ext_transformer parameter to build_yolo_dataset class

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -82,13 +82,14 @@ def seed_worker(worker_id):  # noqa
     random.seed(worker_seed)
 
 
-def build_yolo_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32):
+def build_yolo_dataset(cfg, img_path, batch, data,ext_transform = None ,mode="train", rect=False, stride=32):
     """Build YOLO Dataset."""
     return YOLODataset(
         img_path=img_path,
         imgsz=cfg.imgsz,
         batch_size=batch,
         augment=mode == "train",  # augmentation
+        ext_transform = ext_transform,
         hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
         rect=cfg.rect or rect,  # rectangular batches
         cache=cfg.cache or None,
